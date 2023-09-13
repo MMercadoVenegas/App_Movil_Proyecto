53d60cad814ddaab075a536f72ec21303515720b
Miguel Mercado <migu.mercado@duocuc.cl>
2023-09-12 23:58:59 -0300

    Agregar mensaje de saludo (corregir  mostrado)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'; // Asegúrate de importar NavController si necesitas navegar entre páginas.
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username: string = '';
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
+  ionViewDidEnter() {
+    // Recupera el nombre de usuario del almacenamiento local
+    const storedUserData = localStorage.getItem('user');
 
+    if (storedUserData) {
+      const userData = JSON.parse(storedUserData);
+
+      // Asigna el nombre de usuario a la propiedad 'username' en la clase
+      this.username = userData.username;
+    }
+  }
+
+  logout() {
+    // Elimina la información del usuario del almacenamiento local al cerrar sesión
+    localStorage.removeItem('user');
+    // Navega de vuelta a la página de inicio de sesión
+    this.navCtrl.navigateRoot('/login');
+  }
 }
+
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,12 +12,17 @@ export class RegisterPage {
     password: '',
   };
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   register() {
     // Valida y almacena los datos del usuario en el almacenamiento local
     if (this.userData.username && this.userData.password) {
-      localStorage.setItem('user', JSON.stringify(this.userData));
+      localStorage.setItem('user', JSON.stringify(this.userData))
+      this.router.navigate(['/login']);
+    } else {
+      // Muestra un mensaje de error o realiza acciones adecuadas si los campos no están llenos
     }
   }
 }
+  
+
